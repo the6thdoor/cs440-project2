1edad573caf60adf3f346b055875deafdf0fe0bd
Fernando Gonzalez <fallofrapture59@gmail.com>
2020-07-30 13:35:58 -0400

    Port Haskell code to Python, add ImageType and Mode for more description

--- proj2.py
+++ proj2.py
@@ -1,19 +1,71 @@
 #!/usr/bin/env python
 from os import sys
 from collections import namedtuple
+from enum import Enum
 
-LabeledImage = namedtuple('LabeledImage', ['data', 'label'])
+# A labeled image is a pair of features together with its label.
+LabeledImage = namedtuple('LabeledImage', ['features', 'label'])
 
-# Images are 28x28.
-def print_image(index, data):
+class ImageType(int, Enum):
+    def __new__(cls, value, rows):
+        obj = int.__new__(cls, value)
+        obj._value_ = value
+        obj.rows = rows
+        return obj
+
+    DIGIT = (0, 28)
+    FACE = (1, 70)
+
+class Mode(str, Enum):
+    def __new__(cls, value, path_infix):
+        obj = str.__new__(cls, value)
+        obj._value_ = value
+        obj.path_infix = path_infix
+        return obj
+
+    TRAINING = (0, "training")
+    VALIDATION = (1, "validation")
+    TEST = (2, "test")
+
+def print_image(data, index):
     image = data[index]
-    for i in range(28):
-        print(image[i])
+    for line in image:
+        print(line)
 
-def read_image_data(filename):
-    with open(filename, "r", encoding="utf-8") as f:
+def parse_path(mode, image_type, is_label):
+    suffix = "labels" if is_label else ("images" if image_type == ImageType.DIGIT else "")
+    if image_type == ImageType.DIGIT:
+        return f"data/digitdata/{mode.path_infix}{suffix}"
+    return f"data/facedata/facedata{mode.path_infix}{suffix}"
+
+def read_image_data(mode, image_type):
+    with open(parse_path(mode, image_type, False), "r", encoding="utf-8") as f:
         lines = f.readlines()
-        return [lines[i*28:(i+1)*28] for i in range(len(lines)//28)]
+        number_of_images = len(lines) // image_type.rows
+        return [lines[i*image_type.rows:(i+1)*image_type.rows] for i in range(number_of_images)]
+
+def read_label_data(mode, image_type):
+    with open(parse_path(mode, image_type, True), "r", encoding="utf-8") as f:
+        lines = f.readlines()
+        return map(int, lines)
+
+def extract_features(raw_data):
+    features = []
+    for line in raw_data:
+        for c in line:
+            if c == ' ':
+                features.append(0)
+            elif c == '+':
+                features.append(1)
+            else:
+                features.append(2)
+    return features
+
+def read_labeled_images(mode, image_type):
+    raw_data = read_image_data(mode, image_type)
+    labels = read_label_data(mode, image_type)
+    feature_data = extract_features(raw_data)
+    return [LabeledImage(features, label) for (features, label) in zip(feature_data, labels)]
 
 if __name__ == '__main__':
-    print_image(int(sys.argv[1]), read_image_data("data/digitdata/testimages"))
+    print_image(read_image_data(Mode.TRAINING, ImageType.DIGIT), int(sys.argv[1]))
