af29b66292534776a657b148f0962b99f91fd01b
Fernando Gonzalez <fallofrapture59@gmail.com>
2020-08-03 13:16:05 -0400

    Merged all changes, naive numpy optimization, changed image representation

--- proj2.py
+++ proj2.py
@@ -2,24 +2,41 @@
 from os import sys
 from collections import namedtuple
 from enum import Enum
-import numpy as np
 import math
-# A labeled image is a pair of features together with its label.
-LabeledImage = namedtuple('LabeledImage', ['features', 'label'])
+import numpy as np
+
+# A ProcessedImageData object is a pair of numpy arrays: images processed into features, and labels.
+# It was more useful to organize data this way.
+ProcessedImageData = namedtuple('ProcessedImageData', ['images', 'labels'])
 
 class ImageType(int, Enum):
-    def __new__(cls, value, rows):
+    """A representation of the image type, either digit or face.
+
+    Attributes
+    ----------
+    rows : int
+        the number of rows in the corresponding image type
+    """
+    def __new__(cls, value, rows, categories):
         obj = int.__new__(cls, value)
         obj._value_ = value
         obj.rows = rows
+        obj.categories = categories
         return obj
 
-    DIGIT = (0, 28)
-    FACE = (1, 70)
+    DIGIT = (0, 28, 10)
+    FACE = (1, 70, 2)
 
-class Mode(str, Enum):
+class Mode(int, Enum):
+    """A representation of the mode of the image, either training, validation, or test.
+
+    Attributes
+    ----------
+    path_infix : str
+        the string in the mode's file path which corresponds to the image mode
+    """
     def __new__(cls, value, path_infix):
-        obj = str.__new__(cls, value)
+        obj = int.__new__(cls, value)
         obj._value_ = value
         obj.path_infix = path_infix
         return obj
@@ -29,97 +46,88 @@ class Mode(str, Enum):
     TEST = (2, "test")
 
 def print_image(data, index):
+    """Prints a raw image (array of strings) to stdout."""
     image = data[index]
     for line in image:
         print(line)
 
-def print_labels(labels):
-    print(labels)
-
 def parse_path(mode, image_type, is_label):
+    """Returns the file path to the corresponding image type and mode."""
     suffix = "labels" if is_label else ("images" if image_type == ImageType.DIGIT else "")
     if image_type == ImageType.DIGIT:
         return f"data/digitdata/{mode.path_infix}{suffix}"
-    elif mode.TRAINING:
-        return f"data/facedata/facedatatrain{suffix}"
     return f"data/facedata/facedata{mode.path_infix}{suffix}"
 
 def read_image_data(mode, image_type):
-    with open(parse_path(mode, image_type, False), "r", encoding="utf-8") as f:
-        lines = f.readlines()
+    """Reads raw data from a data file."""
+    with open(parse_path(mode, image_type, False), "r", encoding="utf-8") as datafile:
+        lines = datafile.readlines()
         number_of_images = len(lines) // image_type.rows
-        return [lines[i*image_type.rows:(i+1)*image_type.rows] for i in range(number_of_images)]
+        return np.array([np.array(lines[i*image_type.rows:(i+1)*image_type.rows]) for i in range(number_of_images)])
 
 def read_label_data(mode, image_type):
-    with open(parse_path(mode, image_type, True), "r", encoding="utf-8") as f:
-        return [int(x) for x in f]
+    """Reads labels from a label file."""
+    with open(parse_path(mode, image_type, True), "r", encoding="utf-8") as datafile:
+        return np.array([int(x) for x in datafile])
 
 def extract_features(raw_data):
+    """Parses raw text data into features, represented by an int from 0-2 for the pixel brightness.
+    0 is an empty pixel, 1 is a half-full pixel (+), and 2 is a full pixel (#).
+    """
     features = []
     for line in raw_data:
-        for c in line:
-            if c == ' ':
+        for char in line:
+            if char == ' ':
                 features.append(0)
-            elif c == '+':
+            elif char == '+':
                 features.append(1)
-            else:
+            elif char == '#':
                 features.append(2)
     return features
 
-def read_labeled_images(mode, image_type):
+def read_processed_images(mode, image_type):
     raw_data = read_image_data(mode, image_type)
     labels = read_label_data(mode, image_type)
-    feature_data = extract_features(raw_data)
-    return [LabeledImage(features, label) for (features, label) in zip(feature_data, labels)]
+    feature_data = [extract_features(x) for x in raw_data]
+    return ProcessedImageData(feature_data, labels)
 
-def calcPrior(value, label_data):
+def calc_prior(value, labels):
     #   count each value in the label_data and divide by number of labels to return the prior
-    labels = np.asarray(label_data)
     count = np.count_nonzero(labels == value)
     print(count)
     print(len(labels))
     return count/len(labels)
 
-def calcFeatureProb(feature, index, value, data, label_data):
-    # for calculating the conditional probability of a certain pixel given a value
+def calc_feature_prob(feature, index, value, image_data):
+    # for calculating the conditional probability of a certain pixel given a value)
     i = 0
     count = 0
-    for l in label_data:
+    labels = image_data.labels
+    for l in labels:
         if l == value:
-            image = data[i]
-            if extract_features(image)[index] == feature:
+            image_feature = image_data.images[i][index]
+            if image_feature == feature:
                 count += 1
         i += 1
-    return(count/np.count_nonzero(np.asarray(label_data) == value))
+    return(count/np.count_nonzero(labels == value))
 
-def NaiveBayes(type):
+def naive_bayes(image_type):
     labels = []
-    if(type == "DIGIT"):
-        traindata = read_image_data(Mode.TRAINING, ImageType.DIGIT)
-        trainlabels = read_label_data(Mode.TRAINING, ImageType.DIGIT)
-        validationdata = read_image_data(Mode.VALIDATION, ImageType.DIGIT)
-        validationlabels = read_label_data(Mode.VALIDATION, ImageType.DIGIT)
-        cats = 10
-    else:
-        traindata = read_image_data(Mode.TRAINING, ImageType.FACE)
-        trainlabels = read_label_data(Mode.TRAINING, ImageType.FACE)
-        validationdata = read_image_data(Mode.VALIDATION, ImageType.FACE)
-        validationlabels = read_label_data(Mode.VALIDATION, ImageType.FACE)
-        cats = 2
-    for i in range(len(validationdata)):
-        image = validationdata[i]
+    training_data = read_processed_images(Mode.TRAINING, image_type)
+    validation_data = read_processed_images(Mode.VALIDATION, image_type)
+    for i in range(len(validation_data)):
+        features = validation_data.images[i]
         max = -math.inf
         maxcat = -1
         prob = 0
-        features = extract_features(image)
-        for j in range (cats):
+        for j in range(image_type.categories):
             index = 0
             for f in features:
-                temp = calcFeatureProb(f, index, j, traindata, trainlabels)
+                temp = calc_feature_prob(f, index, j, training_data)
                 if temp != 0:
                     prob += math.log(temp)
                 index += 1
-            temp = (calcPrior(j, trainlabels))
+            temp = calc_prior(j, training_data.labels)
             if temp != 0:
                 print(temp)
                 prob += math.log(temp)
@@ -127,17 +135,12 @@ def NaiveBayes(type):
                 max = prob
                 maxcat = j
         labels.append(maxcat)
-    return (labels)
-
-
-
-
+    return labels
 
 if __name__ == '__main__':
-    #print(calcPrior(5, read_label_data(Mode.TRAINING, ImageType.DIGIT)))
-    print(calcFeatureProb(0,400,9,read_image_data(Mode.TRAINING, ImageType.DIGIT),read_label_data(Mode.TRAINING, ImageType.DIGIT)))
-    print(NaiveBayes("DIGIT"))
-
-
-
-
+    #print(calc_prior(5, read_label_data(Mode.TRAINING, ImageType.DIGIT)))
+    if sys.argv[1] == 'debug':
+        print('hi!')
+    else:
+        print(calc_feature_prob(0, 400, 9, read_processed_images(Mode.TRAINING, ImageType.DIGIT)))
+        print(naive_bayes(ImageType.DIGIT))
