6714be9d055501932f7dba324f18844459f65510
Fernando Gonzalez <fallofrapture59@gmail.com>
2020-08-04 12:21:57 -0400

    numpy optimizationsgit status!

--- proj2.py
+++ proj2.py
@@ -2,12 +2,28 @@
 """Implementation of the Naive Bayes classifier."""
 from collections import namedtuple
 from enum import Enum
-import math
 import numpy as np
 
-# A ProcessedImageData object is a pair of numpy arrays: images processed into features, and labels.
-# It was more useful to organize data this way.
 LabeledImage = namedtuple('LabeledImage', ['features', 'label'])
+
+class ProcessedImageData:
+    """A representation of the image data as a pair of arrays: features and labels.
+    Also provides access to image data in LabeledImage form, to be accessed by index."""
+    def __init__(self, features, labels):
+        self.features = features
+        self.labels = labels
+
+    def __iter__(self):
+        size = min(len(self.features), len(self.labels))
+        for i in range(size):
+            yield LabeledImage(self.features[i], self.labels[i])
+
+    def __getitem__(self, i):
+        return LabeledImage(self.features[i], self.labels[i])
+
+    def __setitem__(self, i, tup):
+        self.features[i] = tup[0]
+        self.labels[i] = tup[1]
 
 class ImageType(int, Enum):
     """A representation of the image type, either digit or face.
@@ -64,90 +80,95 @@ def parse_path(mode, image_type, is_label):
 
 def read_image_data(mode, image_type):
     """Reads raw data from a data file."""
-    with open(parse_path(mode, image_type, False), "r", encoding="utf-8") as datafile:
-        lines = datafile.readlines()
-        num_rows = image_type.rows
-        num_images = len(lines) // num_rows
-        return np.array([(lines[i*num_rows:(i+1)*num_rows]) for i in range(num_images)])
+    txtdata = np.loadtxt(parse_path(mode, image_type, False), dtype=str, delimiter='\n', comments=None)
+    num_rows = image_type.rows
+    num_images = len(txtdata) // num_rows
+    return txtdata.reshape((num_images, num_rows))
 
 def read_label_data(mode, image_type):
     """Reads labels from a label file."""
-    with open(parse_path(mode, image_type, True), "r", encoding="utf-8") as datafile:
-        return np.array([int(x) for x in datafile])
+    return np.loadtxt(parse_path(mode, image_type, True), dtype=int, delimiter='\n')
+
+def extract_features_debug(raw_data):
+    """Parses raw text data into features, represented by an int from 0-2 for the pixel brightness.
+    0 is an empty pixel, 1 is a half-full pixel (+), and 2 is a full pixel (#).
+    """
+    width = len(raw_data[0])
+    num_features = len(raw_data) * width
+    features = np.zeros((num_features, 3), dtype=int)
+    for row, line in enumerate(raw_data):
+        for col, char in enumerate(line):
+            if char == ' ':
+                features[col + row * width] = 0
+            elif char == '+':
+                features[col + row * width] = 1
+            elif char == '#':
+                features[col + row * width] = 2
+    return features
+
 
 def extract_features(raw_data):
     """Parses raw text data into features, represented by an int from 0-2 for the pixel brightness.
     0 is an empty pixel, 1 is a half-full pixel (+), and 2 is a full pixel (#).
     """
-    features = []
-    for line in raw_data:
-        for char in line:
+    width = len(raw_data[0])
+    num_features = len(raw_data) * width
+    features = np.zeros((num_features, 3), dtype=bool)
+    for row, line in enumerate(raw_data):
+        for col, char in enumerate(line):
             if char == ' ':
-                features.append(0)
+                features[col + row * width][0] = True
             elif char == '+':
-                features.append(1)
+                features[col + row * width][1] = True
             elif char == '#':
-                features.append(2)
-    return np.array(features)
+                features[col + row * width][2] = True
+    return features
 
-def read_labeled_images(mode, image_type):
+def read_processed_images(mode, image_type):
     """Reads data and label files and compiles them into an array of labeled images."""
     raw_data = read_image_data(mode, image_type)
     labels = read_label_data(mode, image_type)
-    feature_data = [extract_features(x) for x in raw_data]
-    return [LabeledImage(features, label) for (features, label) in zip(feature_data, labels)]
+    features = np.apply_along_axis(extract_features, 1, raw_data)
+    return ProcessedImageData(features, labels)
 
-def calc_prior(value, data):
-    """Counts each value in the label data and divides by number of labels to return the prior."""
-    labels = np.array([label for (_, label) in data])
-    count = np.count_nonzero(labels == value)
-    print(count)
-    print(len(labels))
-    return count/len(labels)
+def calc_priors(categories, data):
+    """Calculates the prior probabilities of each label by counting the occurrences of the label
+       and dividing by the total number of labels."""
+    counts = np.zeros(categories)
+    for val in range(categories):
+        counts[val] = np.count_nonzero(data.labels == val)
+    return counts / len(data.labels)
 
-def calc_feature_probs(value, image_data, smoothing):
+def calc_feature_probs(categories, image_data, smoothing):
     """Computes the conditional probability for each feature-pixel pair, given a certain label."""
-    value_images = [image.features for image in image_data if image.label == value]
-    total_occurrences = len(value_images)
-    total_pixels = len(value_images[0])
-    counts = np.full((3, total_pixels), smoothing)
-    for feature in range(3):
-        for image_index in range(total_occurrences):
-            for pixel_index in range(len(value_images[0])):
-                if value_images[image_index][pixel_index] == feature:
-                    counts[feature][pixel_index] += 1
-    denominator = total_occurrences + (smoothing * 3)
-    return np.array([count_sub / denominator for count_sub in counts])
+    counts = np.array([np.sum(image_data.features[image_data.labels == value], axis=0) + smoothing for value in range(categories)])
+    denoms = np.array([np.count_nonzero(image_data.labels == value) + (smoothing * 3) for value in range(categories)])
+    return counts / denoms[:, np.newaxis, np.newaxis]
 
 def train_naive_bayes(image_type, smoothing):
     """Uses the training data of the image type to assemble Bayesian probability data."""
+    print('Loading training data...')
+    training_data = read_processed_images(Mode.TRAINING, image_type)
     print('Training classifier...')
-    training_data = read_labeled_images(Mode.TRAINING, image_type)
-    conditionals = [calc_feature_probs(val, training_data, smoothing) for val in range(image_type.categories)]
-    priors = np.array([calc_prior(val, training_data) for val in range(image_type.categories)])
+    conditionals = calc_feature_probs(image_type.categories, training_data, smoothing)
+    priors = calc_priors(image_type.categories, training_data)
     print(f'Trained classifier for image type = {image_type.name}')
     return {'image_type': image_type, 'conditionals': conditionals, 'priors': priors}
 
 def classify_naive_bayes(classifier_data, mode, indices):
     """Classifies the given images using the given probability data."""
-    image_data = read_labeled_images(mode, classifier_data['image_type'])
+    image_data = read_processed_images(mode, classifier_data['image_type'])
     labels = []
+    log_priors = np.log(classifier_data['priors'])
+    log_conditionals = np.log(classifier_data['conditionals'])
+    categories = classifier_data['image_type'].categories
     for i in indices:
-        prob_max = -math.inf
-        maxcat = None
-        for current_label in range(classifier_data['image_type'].categories):
-            prob = 0
-            for index, feature in enumerate(image_data[i].features):
-                cond_prob = classifier_data['conditionals'][current_label][feature][index]
-                if cond_prob != 0:
-                    prob += math.log(cond_prob)
-            prior = classifier_data['priors'][current_label]
-            if prior != 0:
-                prob += math.log(prior)
-            if prob > prob_max:
-                prob_max = prob
-                maxcat = current_label
-        labels.append((i, maxcat))
+        probabilities = np.zeros(categories)
+        for cur_label in range(categories):
+            log_prior = log_priors[cur_label]
+            log_conds = np.sum(log_conditionals[cur_label, image_data[i].features])
+            probabilities[cur_label] = log_prior + log_conds
+        labels.append((i, np.argmax(probabilities)))
     for index, label in labels:
         print(f'Image {index} classified as: {label}')
     return labels
